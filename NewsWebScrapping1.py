8a825ba4fcbadfc1dae5bdb129854da491329607
rohitbansal123 <bansalrohit627@gmail.com>
2020-03-26 09:34:23 +0530

    news web scrapping 2 file added

--- NewsWebScrapping1.py
+++ NewsWebScrapping1.py
@@ -1,7 +1,10 @@
+#import nltk
+#nltk.download('punkt')
+
 from newspaper import Article
 
 #A new article from TOI
-url = "https://timesofindia.indiatimes.com/world/china/chinese-expert-warns-of-troops-entering-kashmir/articleshow/59516912.cms"
+url = "https://timesofindia.indiatimes.com/india/mahabharata-battle-won-in-18-days-war-against-coronavirus-will-take-21-days-pm-modi/articleshow/74813107.cms"
 
 #For different language newspaper refer above table
 toi_article = Article(url, language="en") # en for English
@@ -18,18 +21,33 @@ toi_article.nlp()
 #To extract title
 print("Article's Title:")
 print(toi_article.title)
-print("n")
+print("\n")
+
+#To extract publish date
+print("Article Publish Date:")
+print(toi_article.publish_date)
+print("\n")
+
+#To extract authors
+print("Article's authors:")
+print(toi_article.authors)
+print("\n")
 
 #To extract text
 print("Article's Text:")
 print(toi_article.text)
-print("n")
+print("\n")
 
 #To extract summary
 print("Article's Summary:")
 print(toi_article.summary)
-print("n")
+print("\n")
 
 #To extract keywords
 print("Article's Keywords:")
 print(toi_article.keywords)
+print("\n")
+#To extract HTML
+print("Article's HTML:")
+#print(toi_article.html)
+toi_article.html
