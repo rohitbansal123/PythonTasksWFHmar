6e7bd6e9beef9505b85991270a7e2f6c6e6aa02f
Rohit Bansal <52486648+rohitbansal123@users.noreply.github.com>
2020-04-09 15:37:48 +0530

    Update ArgparseInputFileProcessing.py

--- ArgparseInputFileProcessing.py
+++ ArgparseInputFileProcessing.py
@@ -7,22 +7,13 @@ mycursor=conn.cursor()#cursor() method create a cursor object
 
 parser = argparse.ArgumentParser(description= "input CSV values from txt and load these in a table ")
 parser.add_argument("input", help = "Input File")
-#parser.add_argument("--output", help = "Output File")
+
 args = parser.parse_args()
 
 print('inputfile:', args.input)
-#print('outputfile:', args.output)
+
 ain = args.input
 print(ain)
-"""
-f = open(ain,'r')
-
-for a in range (1,5):
-    for x in f.readline().split(',') :
-        print(x)
-
-f.close()
-"""
 # new things added for csv to mysql table loading of data
 csv_file = open(ain)
 csv_data = csv.reader(csv_file)
